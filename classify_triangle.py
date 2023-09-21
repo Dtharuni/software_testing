540a942a4212e7555ad2fa7ed829a6f1eb1ebd52
Dtharuni <144611337+Dtharuni@users.noreply.github.com>
2023-09-20 22:06:27 -0400

    Update classify_triangle.py

--- classify_triangle.py
+++ classify_triangle.py
@@ -1,5 +1,18 @@
 import unittest
 import math
+import datetime
+
+def my_brand(hw_name):
+    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    screen_brand = f"=*=*=*= Tharuni D =*=*=*=\n\n" \
+                  f"=*=*=*= Course 2023S-SSW567-WS =*=*=*=\n\n" \
+                  f"=*=*=*= {hw_name} =*=*=*=\n\n" \
+                  f"=*=*=*= {current_datetime} =*=*=*=\n"
+    
+    print(screen_brand)
+
+my_brand("HW 00")
 
 class TriangleClassifier:
     def __init__(self, side_a, side_b, side_c):
