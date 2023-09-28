f0075ab2799ad4a9d17ed03a399c1d2902e6d96f
Tharuni Diddekunta <tdiddeku@stevens.edu>
2023-09-27 21:57:54 -0400

    TestTriangle

--- TestTriangle.py
+++ TestTriangle.py
@@ -3,27 +3,27 @@ from Triangle import TriangleClassifier
 
 class TestTriangleClassifier(unittest.TestCase):
     def test_equilateral_triangle(self):
-        triangle = TriangleClassifier(5, 5, 5)
+        Triangle = TriangleClassifier(5, 5, 5)
         self.assertEqual(triangle.classify_triangle(), "Equilateral")
         print("test_equilateral_triangle is passed")
 
     def test_isosceles_triangle(self):
-        triangle = TriangleClassifier(5, 5, 8)
+        Triangle = TriangleClassifier(5, 5, 8)
         self.assertEqual(triangle.classify_triangle(), "Isosceles")
         print("test_isosceles_triangle is passed")
 
     def test_scalene_triangle(self):
-        triangle = TriangleClassifier(3, 4, 5)
+        Triangle = TriangleClassifier(3, 4, 5)
         self.assertEqual(triangle.classify_triangle(), "Scalene")
         print("test_scalene_triangle is passed")
 
     def test_right_triangle(self):
-        triangle = TriangleClassifier(3, 4, 5)
+        Triangle = TriangleClassifier(3, 4, 5)
         self.assertEqual(triangle.classify_triangle(), "Right")
         print("test_right_triangle is passed")
 
     def test_invalid_triangle(self):
-        triangle = TriangleClassifier(0, 0, 0)
+        Triangle = TriangleClassifier(0, 0, 0)
         self.assertEqual(triangle.classify_triangle(), "Not a valid triangle")
 
 if __name__ == "__main__":
